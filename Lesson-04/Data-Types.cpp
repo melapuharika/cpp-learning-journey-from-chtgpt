5338cf9504f595c4e48f5a37fb946f833fcf23df
melapuharika <melapuharika@gmail.com>
2026-09-07 01:31:53 +0530

    Update and rename Data-Types.cpp to Data-Types.cpp

--- Lesson-04/Data-Types.cpp
+++ Lesson-04/Data-Types.cpp
@@ -15,3 +15,42 @@ wchar_t
 char8_t
 char16_t
 char32_t
+
+
+## topic 1: int
+
+- "int" is short for integer.
+- It is used to store whole numbers.
+- Whole numbers can be positive, negative, or zero.
+- "int" normally does not store decimal values.
+
+Syntax
+
+int variable_name = value;
+
+Examples
+
+int age = 22;
+int marks = 95;
+int number = -10;
+int count = 0;
+
+Example Program
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 22;
+
+    cout << age;
+
+    return 0;
+}
+
+Output
+
+22
+
+Remember: "int" → Whole numbers.
